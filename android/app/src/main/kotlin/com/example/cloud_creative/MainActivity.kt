5cdb5cd092932e36dba358e4489e9ec666765536
jasim <jasimrony50@gmail.com>
2025-03-08 23:47:57 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/cloud_creative/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cloud_creative
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/home/home_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Hello"),
+        centerTitle: true,
+        actions: [Icon(Icons.shopping_bag)],
+      ),
+      drawer: Drawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Image.asset("assets/app_images/Theme.png"),
+            Text(
+              "Maximum Note 8i",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+                "Powered by a 10nm octa-core Qualcomm Snapdragon 712 AIE processor this phone can seamlessly execute any task. You can game more and multitask without experience any any lag. Experience a boost in performance and efficiency with this phone")
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
